4376672262136c343d59ceb648e72dad61006ae9
infastin <infastin@yandex.com>
2025-03-31 19:12:07 +0500

    fix(core): potential sender loop deadlock

--- pkg/core/track.go
+++ pkg/core/track.go
@@ -153,12 +153,19 @@ func (s *Sender) Start() {
 }
 
 func (s *Sender) Wait() {
-	if done := s.done; done != nil {
+	s.mu.Lock()
+	done := s.done
+	s.mu.Unlock()
+
+	if done != nil {
 		<-done
 	}
 }
 
 func (s *Sender) State() string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	if s.buf == nil {
 		return "closed"
 	}
