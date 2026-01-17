9a28a3c9f13d5ba63bfef531cd68887d32318654
seydx <dev@seydx.com>
2026-01-17 23:29:58 +0100

    bump version to 1.9.13-cui.18

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ import (
 
 func main() {
 	// version will be set later from -buildvcs info, this used only as fallback
-	app.Version = "1.9.13-cui.17"
+	app.Version = "1.9.13-cui.18"
 
 	type module struct {
 		name string
