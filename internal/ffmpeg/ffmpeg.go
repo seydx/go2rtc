fb8cdae63da52b27428170b4f5f351194785b715
seydx <dev@seydx.com>
2026-01-20 17:14:28 +0100

    format

--- internal/ffmpeg/ffmpeg.go
+++ internal/ffmpeg/ffmpeg.go
@@ -126,7 +126,7 @@ var defaults = map[string]string{
 	"pcml/16000": "-c:a pcm_s16le -ar:a 16000 -ac:a 1",
 	"pcml/44100": "-c:a pcm_s16le -ar:a 44100 -ac:a 1",
 	"pcml/48000": "-c:a pcm_s16le -ar:a 48000 -ac:a 1",
-	"g726":    "-c:a g726 -b:a 40k -ar:a 8000 -ac:a 1",
+	"g726":       "-c:a g726 -b:a 40k -ar:a 8000 -ac:a 1",
 	"g726/16000": "-c:a g726 -b:a 40k -ar:a 16000 -ac:a 1",
 	"g726/48000": "-c:a g726 -b:a 40k -ar:a 48000 -ac:a 1",
 
