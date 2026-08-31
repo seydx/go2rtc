cdc28fb5613a9b2a3afd2c0f5b93a46519276d52
seydx <hi@seydx.dev>
2026-08-31 18:47:56 +0200

    chore: bump version to 1.9.14-cui.40

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ import (
 
 func main() {
 	// version will be set later from -buildvcs info, this used only as fallback
-	app.Version = "1.9.14-cui.39"
+	app.Version = "1.9.14-cui.40"
 
 	type module struct {
 		name string
